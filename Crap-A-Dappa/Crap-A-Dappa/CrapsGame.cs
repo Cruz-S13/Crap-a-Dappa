233fdb8550ca2d79b13c2b138898b5231934c220
John Cruz <77587169+Cruz-S13@users.noreply.github.com>
2021-03-12 17:24:47 -0800

    Add files via upload

--- Crap-A-Dappa/Crap-A-Dappa/CrapsGame.cs
+++ Crap-A-Dappa/Crap-A-Dappa/CrapsGame.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace Crap_A_Dappa
 {
+	/*
+	 The 'CrapsGame' class consists of 2 enum data types and 5 methods, of which are the backbone 
+	 of the program. This class utilizes the enums to either name special sums of the dice rolled, 
+	 or contains the status of the game(s) until you are no longer in play. Switches are also used 
+	 within this class to either evaluate the dice rolls where players will either win or lose on 
+	 the first roll, or continue playing up to the desired amount of rolls.
+	 */
 	public class CrapsGame
 	{
-		private enum DiceSum
+		private enum DiceSum //Defines the names for each sum of special roll IF these are the first roll of each game.
 		{
 			SnakeEyes = 2,
 			Trey = 3,
@@ -16,20 +23,20 @@ namespace Crap_A_Dappa
 			YoLeven = 11,
 			BoxCars = 12
 		};
-		public enum GameStatus
+		public enum GameStatus //Defines the game status of either Win, Lose, or Continue.
 		{
 			Win,
 			Lose,
 			Continue
 		};
-		private RollDice _roll;
-		private GameStatus _gameStatus;
-		private DiceSum _diceSum;
-		private int _numRolls;
-		private int _sum;
-		private int _point;
-		public const int numGames = 5000;
-		private Statistics _stats;
+		private RollDice _roll; 
+		private GameStatus _gameStatus; 
+		private DiceSum _diceSum; 
+		private int _numRolls; 
+		private int _sum; 
+		private int _point; 
+		public const int numGames = 500; //Edit this if you want to roll a different number of games.
+		private Statistics _stats; 
 
 		public CrapsGame()
 		{
@@ -37,7 +44,7 @@ namespace Crap_A_Dappa
 			_stats = new Statistics();
 		}
 
-		private void EvaluateRoll()
+		private void EvaluateRoll() // Defines switches if specific sums of the die are rolled on the first roll of the game. These are win/lose rolls.
 		{
 			switch (_sum)
 			{
@@ -78,19 +85,19 @@ namespace Crap_A_Dappa
 			}
 		}
 
-		private void DisplayMessage()
+		private void DisplayMessage() //These Switches are used to display the game status to the player. 
 		{
 			switch (_gameStatus)
 			{
 				case GameStatus.Win:
 					Console.WriteLine(_numRolls == 1
-						? $"Congratulations, you rolled {_diceSum}. YOU WIN!"
-						: $"Congratulations, you rolled {_sum}. YOU WIN!");
+						? $"Congratulations, you rolled {_diceSum}. YOU WIN!\n"
+						: $"Congratulations, you rolled {_sum}. YOU WIN!\n");
 					break;
 				case GameStatus.Lose:
 					Console.WriteLine(_numRolls == 1
-						? $"Sorry, you rolled {_diceSum}. YOU LOSE!"
-						: $"Congratulations, you rolled {_sum}. YOU WIN!");
+						? $"Sorry, you rolled {_diceSum}. YOU LOSE!\n"
+						: $"Congratulations, you rolled {_sum}. YOU WIN!\n");
 					break;
 				default:
 					Console.WriteLine($"You rolled {_sum}. Your point is {_point}. Keep rolling!");
@@ -98,7 +105,7 @@ namespace Crap_A_Dappa
 			}
 		}
 
-		public void Play()
+		public void Play() //this is used to display the current game, and each roll of given game until you either win/lose.
 		{
 			for (int i = 0; i < numGames; i++)
 			{
@@ -122,7 +129,7 @@ namespace Crap_A_Dappa
 
 
 
-		public void KeepPlaying()
+		public void KeepPlaying() //this is used to provide the special conditions for each game.
 		{
 			_sum = _roll.DiceRoll();
 			_numRolls++;
